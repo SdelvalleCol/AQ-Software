65a01caa6b77e688efb1742967f8f9bcbddbee70
Santiago Andres Del Valle Pinilla <santiagoandresdelvallepinilla@gmail.com>
2023-04-18 17:36:31 -0500

    Update

--- app/backend/BD/config.js
+++ app/backend/BD/config.js
@@ -3,10 +3,10 @@ const mysql = require('mysql');
 const { promisify } = require('util');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "aq"
 });
 
 pool.getConnection((error, connection) => {

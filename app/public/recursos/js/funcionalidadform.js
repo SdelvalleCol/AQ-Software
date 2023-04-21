eae26757bc97bf72a6d136f60d228c78b06851e8
Santiago Andres Del Valle Pinilla <santiagoandresdelvallepinilla@gmail.com>
2023-04-21 14:42:57 -0500

    Update

---
+++ app/public/recursos/js/funcionalidadform.js
@@ -0,0 +1,17 @@
+var iscoche;
+
+
+function iscoche_def(){
+    if(document.getElementById("cbox1").checked){
+        iscoche = true
+    }else{
+        iscoche = false
+    }
+}
+
+function mostrarcoch(){
+    iscoche_def()
+    if(iscoche){
+
+    }
+}

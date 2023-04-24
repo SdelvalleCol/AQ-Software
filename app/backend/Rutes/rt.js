1d9ed40f13a315edcc9f5b37bb474514fd04b7c5
Santiago Andres Del Valle Pinilla <santiagoandresdelvallepinilla@gmail.com>
2023-04-23 20:55:39 -0500

    Update
    23/04/2023

--- app/backend/Rutes/rt.js
+++ app/backend/Rutes/rt.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const pool = require('../BD/config.js');
 const token = require('jsonwebtoken')
 
-
 //RUTAS VISTAS
 router.get('/', (req, res) => {
   res.render('main', { navbar: 'navbar' });
@@ -22,20 +21,87 @@ router.get('/inicio/ingreso', (req, res) => {
   res.render('ver_ingresos', { navbar: 'navbar' });
 });
 
-
-
 //RUTAS APIS
 
+//Registrar usuario
 router.post('/registrar/usuario', (req, res) => {
   const cuerpo = req.body;
-  try{
-    pool.query(`CALL insertar_usuario(${cuerpo.cedula},'${cuerpo.nombre}',${cuerpo.edad},${cuerpo.telefono},'${cuerpo.sexo}',${cuerpo.tipo})`)
-    res.json(0)
-  }catch(e){
+  try {
+    pool.query(`SELECT ver_usuario(${cuerpo.cedula})`, (error, resultado) => {
+      const respuesta = resultado[0];
+      const valor = JSON.parse(respuesta["ver_usuario(" + cuerpo.cedula + ")"]);
+      const id_final = valor["persona_id"]
+      if (id_final == null) {
+        pool.query(`CALL insertar_usuario(${cuerpo.cedula},'${cuerpo.nombre}',${cuerpo.edad},${cuerpo.telefono},'${cuerpo.sexo}',${cuerpo.tipo})`)
+      }else{
+        res.json("Usuario ya creado")
+      }
+    })
+  } catch (e) {
     console.log(e)
     res.json(0)
   }
+});
 
+//Registrar visita persona
+router.post('/registrar/usuario/visita', (req, res) => {
+  const cuerpo = req.body;
+  try {
+    pool.query(`SELECT ver_usuario(${cuerpo.cedula})`, (error, resultado) => {
+      const respuesta = resultado[0];
+      const valor = JSON.parse(respuesta["ver_usuario(" + cuerpo.cedula + ")"]);
+      const id_final = valor["persona_id"]
+      const tipo = valor["tipo_persona"]
+      if (id_final != null) {
+        pool.query(`CALL insertar_registro(${id_final},${tipo},${0})`, (error2, resultado2) => {
+          if (error2) {
+            console.log(error2)
+          } else {
+            res.json("Yeah")
+          }
+        })
+      } else {
+        res.json("No existe")
+      }
+    })
+  } catch (e) {
+    console.log(e)
+    res.json(0)
+  }
+});
+
+//Registrar visita vehiculo y usuario
+router.post('/registrar/vehiculo/visita', (req, res) => {
+  const cuerpo = req.body;
+  try {
+    pool.query(`SELECT ver_usuario(${cuerpo.cedula})`, (error, resultado) => {
+      const respuesta = resultado[0];
+      const valor = JSON.parse(respuesta["ver_usuario(" + cuerpo.cedula + ")"]);
+      const id_final = valor["persona_id"]
+      const tipo = valor["tipo_persona"]
+      if (id_final != null) {
+        pool.query(`CALL insertar_registro_coche('${cuerpo.matricula}','${cuerpo.ciudad}',${cuerpo.cedula})`, (error2, resultado2) => {
+          if (error2) {
+            res.json("ERROR")
+          } else {
+            pool.query(`CALL insertar_registro(${id_final},${tipo},'${cuerpo.matricula}')`, (error3, resultado3) => {
+              if (error3) {
+                console.log(error3)
+              } else {
+                res.json("Exito")
+              }
+            })
+          }
+        })
+
+      } else {
+        res.json("No existe")
+      }
+    })
+  } catch (e) {
+    console.log(e)
+    res.json(0)
+  }
 });
 
 
--- app/public/recursos/js/funcionalidadform.js
+++ app/public/recursos/js/funcionalidadform.js
@@ -18,7 +18,18 @@ function mostrarcoch() {
     }
 }
 
-//Registrar personas vista formulario
+function obtener_parametros() {
+    var cont = 0
+    const elementos = document.getElementsByClassName('input_form');
+    for (var i = 0; i < elementos.length; i++) {
+        if (elementos[i].value != null && elementos[i].value != "") {
+            cont++
+        }
+    }
+    return cont
+}
+
+//Registrar personas 
 async function registrar_persona() {
     const elementos = document.getElementsByClassName('input_form');
     const contador = obtener_parametros()
@@ -37,3 +48,48 @@ async function registrar_persona() {
     }
 }
 
+//Registrar visita personas
+async function registrar_visita() {
+    var cedula = document.getElementById("cedula_inp").value
+    var matricula = document.getElementById("matricula_inp").value
+    var ciudad = document.getElementById("ciudad_inp").value
+    if (cedula) {
+        if (iscoche) {
+            if (matricula != "" && ciudad != "") {
+                const data ={cedula:cedula, matricula : matricula , ciudad:ciudad}
+                //Registrar entrada y coche
+                    await fetch('http://localhost:5000/registrar/vehiculo/visita', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data)
+                })
+                    .then(response => response.json())
+                    .then(data_p => console.log(data_p))
+                    .catch(error => console.error(error));
+
+
+            } else {
+                console.log("Rellene los datos")
+            }
+        } else {
+            //Registro entrada
+            const data = { cedula: cedula }
+            await fetch('http://localhost:5000/registrar/usuario/visita', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+                .then(response => response.json())
+                .then(data_p => console.log(data_p))
+                .catch(error => console.error(error));
+        }
+    } else {
+        console.log("Rellene los datos")
+    }
+
+
+}
